ad006235cdcf4ec5eaa0f40cdf7a7adce7c918fc
LaysonEduardo <layson.contato@gmail.com>
2023-05-08 03:41:51 -0300

    feat: added app_weather_icons to manage weather icons

--- lib/src/common/app_weather_icons.dart
+++ lib/src/common/app_weather_icons.dart
@@ -4,30 +4,63 @@ import 'package:salary_fits_test/src/services/time/time_services.dart';
 import '../services/weather/weather_state.dart';
 
 class AppWeatherIcons {
-  Widget icon(WeatherState weatherState) {
+  static Widget humidity({double size = 150}) {
+    return Image.asset('assets/icons/humidity.png');
+  }
+
+  static Widget wind({double size = 150}) {
+    return Image.asset('assets/icons/wind.png');
+  }
+
+  static Widget weatherIcon(
+      {required WeatherState weatherState, double size = 150}) {
     switch (weatherState) {
       case WeatherState.clearSky:
         return TimeServices.isDay()
-            ? Image.asset('assets/icons/clear_day.png')
-            : Image.asset('assets/icons/clear_night.png');
+            ? Image.asset(
+                'assets/icons/clear_day.png',
+                width: 150,
+              )
+            : Image.asset(
+                'assets/icons/clear_night.png',
+                width: 150,
+              );
 
       case WeatherState.cloudy:
-        return Image.asset('assets/icons/cloudy.png');
+        return Image.asset(
+          'assets/icons/cloudy.png',
+          width: 150,
+        );
 
       case WeatherState.drizzle:
-        return Image.asset('assets/icons/rain.png');
+        return Image.asset(
+          'assets/icons/rain.png',
+          width: 150,
+        );
 
       case WeatherState.rain:
-        return Image.asset('assets/icons/rain.png');
+        return Image.asset(
+          'assets/icons/rain.png',
+          width: 150,
+        );
 
       case WeatherState.thunderStorm:
-        return Image.asset('assets/icons/thunderstorm.png');
+        return Image.asset(
+          'assets/icons/thunderstorm.png',
+          width: 150,
+        );
 
       case WeatherState.snow:
-        return Image.asset('assets/icons/snow.png');
+        return Image.asset(
+          'assets/icons/snow.png',
+          width: 150,
+        );
 
       default:
-        return Image.asset('assets/icons/clear_sky.png');
+        return Image.asset(
+          'assets/icons/clear_sky.png',
+          width: 150,
+        );
     }
   }
 }
