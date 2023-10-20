f3d53ee66eb5f29b1e58cbff5e0e9436f71395f6
LaysonEduardo <layson.contato@gmail.com>
2023-10-20 09:05:00 -0300

    fix: test fix

---
+++ lib/main_dev.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+import 'package:salary_fits_test/app/app_module.dart';
+import 'app/app_widget.dart';
+import 'src/services/UI/ui_services.dart';
+
+void main() async {
+  await UIServices.init();
+  runApp(
+    ModularApp(
+      module: AppModule(),
+      child: const WeatherApp(),
+    ),
+  );
+}
